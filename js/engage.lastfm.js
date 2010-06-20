ac305fae922667ca508e215da22f35ac2a2e9db8
Joshua Priddle <jpriddle@nevercraft.net>
2010-06-20 18:39:19 -0400

    Update JS

--- js/engage.lastfm.js
+++ js/engage.lastfm.js
@@ -10,79 +10,79 @@
 ---------------*/
 
 (function($){
-	$.fn.lastFM = function(options) {
-		
-		var defaults = {
-			number: 10,
-			username: 'willblackmore',
-			apikey: '96e0589327a3f120074f74dbc8ec6443',
-			artSize: 'medium',
-			noart: 'images/noartwork.gif',
-			onComplete: function(){}
-		},
-		settings = $.extend({}, defaults, options);
+  $.fn.lastFM = function(options) {
 
-		var lastUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+settings.username+'&api_key='+settings.apikey+'&limit='+settings.number+'&format=json&callback=?';
-		var $this = $(this);
-		
-		var container = $this.html();
-		
-		$this.children(':first').remove();
-		
-		if(settings.artSize == 'small'){imgSize = 0}
-		if(settings.artSize == 'medium'){imgSize = 1}
-		if(settings.artSize == 'large'){imgSize = 2}
+    var defaults = {
+      number: 10,
+      username: '',
+      apikey: '',
+      artSize: 'medium',
+      noart: 'images/noartwork.gif',
+      onComplete: function(){}
+    },
+    settings = $.extend({}, defaults, options);
 
-		this.each(function() {
-			
-			$.getJSON(lastUrl, function(data){ 
-				$.each(data.recenttracks.track, function(i, item){
+    var lastUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+settings.username+'&api_key='+settings.apikey+'&limit='+settings.number+'&format=json&callback=?';
+    var $this = $(this);
 
-					if(item.image[1]['#text'] == ''){
-						art = settings.noart;
-					}else{
-						art = stripslashes(item.image[imgSize]['#text']);
-					}
+    var container = $this.html();
 
-					url = stripslashes(item.url);
-					song = item.name;
-					artist = item.artist['#text'];
-					album = item.album['#text'];
-					
-					nowplaying = item['@attr'] && item['@attr']['nowplaying'] || false;
+    $this.children(':first').remove();
 
-					if (nowplaying) {
-					  //song += ' <span class="nowplaying"><image src="images/icon_eq.gif" /> Listening Now</span>';
-					  song += ' <span class="nowplaying"><image src="images/icon_eq.gif" /> a</span>';
-					}
+    if(settings.artSize == 'small'){imgSize = 0}
+    if(settings.artSize == 'medium'){imgSize = 1}
+    if(settings.artSize == 'large'){imgSize = 2}
+
+    this.each(function() {
+
+      $.getJSON(lastUrl, function(data){
+        $.each(data.recenttracks.track, function(i, item){
+
+          if(item.image[1]['#text'] == ''){
+            art = settings.noart;
+          }else{
+            art = stripslashes(item.image[imgSize]['#text']);
+          }
+
+          url = stripslashes(item.url);
+          song = item.name;
+          artist = item.artist['#text'];
+          album = item.album['#text'];
+
+          nowplaying = item['@attr'] && item['@attr']['nowplaying'] || false;
+
+          if (nowplaying) {
+            //song += ' <span class="nowplaying"><image src="images/icon_eq.gif" /> Listening Now</span>';
+            song += ' <span class="nowplaying"><image src="images/icon_eq.gif" /> a</span>';
+          }
 
           if (album != "") {
             album = ' - ' + album;
           }
 
-					$this.append(container);
-					
-					var $current = $this.children(':eq('+i+')');
-					
-					$current.find('[class=lfm_song]').append(song);
-					$current.find('[class=lfm_artist]').append(artist);
-					$current.find('[class=lfm_album]').append(album);
+          $this.append(container);
+
+          var $current = $this.children(':eq('+i+')');
+
+          $current.find('[class=lfm_song]').append(song);
+          $current.find('[class=lfm_artist]').append(artist);
+          $current.find('[class=lfm_album]').append(album);
           // $current.find('[class=lfm_art]').append("<img src='"+art+"' alt='Artwork for "+album+"'/>");
           $current.find('[class=lfm_overlay]').replaceWith("<img id='icon' src='"+art+"' alt='Artwork for "+album+"' class='lfm_overlay'/>");
           // $current.find('a').attr('href', url).attr('title', 'Listen to '+song+' on Last.FM').attr('target', '_blank');
-					
-					//callback
-					if(i==(settings.number-1)){
-						settings.onComplete.call(this);
-					}
-					
-				});
-			});
-		});
-	};
-	
-	//Clean up the URL's
-	function stripslashes( str ) {	 
-		return (str+'').replace(/\0/g, '0').replace(/\\([\\'"])/g, '$1');
-	}
+
+          //callback
+          if(i==(settings.number-1)){
+            settings.onComplete.call(this);
+          }
+
+        });
+      });
+    });
+  };
+
+  //Clean up the URL's
+  function stripslashes( str ) {
+    return (str+'').replace(/\0/g, '0').replace(/\\([\\'"])/g, '$1');
+  }
 })(jQuery);
